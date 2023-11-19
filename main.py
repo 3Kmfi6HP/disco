affe543f7fd6d0c8e28e094f339c864ba6b3e6e2
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-11-19 19:45:19 +0700

    add

--- main.py
+++ main.py
@@ -10,95 +10,115 @@ import feedparser
 import subprocess
 
 
-def process_fofa_file(url, output_file="merge-ip.txt"):
-    """
-    Downloads a file from the given URL and processes it.
+# def process_fofa_file(url, output_file="merge-ip.txt"):
+#     """
+#     Downloads a file from the given URL and processes it.
 
-    Parameters:
-    - url (str): The URL of the file to be downloaded.
-    - output_file (str): The name of the output file (default is "merge-ip.txt").
+#     Parameters:
+#     - url (str): The URL of the file to be downloaded.
+#     - output_file (str): The name of the output file (default is "merge-ip.txt").
 
-    Returns:
-    None
-    """
-    response = requests.get(url)
-    # Add your processing logic here
+#     Returns:
+#     None
+#     """
+#     response = requests.get(url)
+#     # Add your processing logic here
 
-    if response.status_code != 200:
-        print("无法下载文件")
-        return
+#     if response.status_code != 200:
+#         print("无法下载文件")
+#         return
 
-    data = response.text
+#     data = response.text
 
-    # 提取IP地址和端口号
-    result = []
-    lines = data.split("\n")
-    for line in lines:
-        line = line.strip()
-        if not line:
-            continue
+#     # 提取IP地址和端口号
+#     result = []
+#     lines = data.split("\n")
+#     for line in lines:
+#         line = line.strip()
+#         if not line:
+#             continue
 
-        # 去掉http://和https://
-        line = re.sub(r"^https?://", "", line)
+#         # 去掉http://和https://
+#         line = re.sub(r"^https?://", "", line)
 
-        # 如果没有端口号，使用默认端口
-        if ":" not in line:
-            line = f"{line}:443"  # 默认端口为443
+#         # 如果没有端口号，使用默认端口
+#         if ":" not in line:
+#             line = f"{line}:443"  # 默认端口为443
 
-        # 提取IP地址和端口号
-        ip_port = line.split(":")
-        if len(ip_port) == 2:
-            ip, port = ip_port
-            if re.match(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", ip):
-                result.append(f"{ip} {port}")
+#         # 提取IP地址和端口号
+#         ip_port = line.split(":")
+#         if len(ip_port) == 2:
+#             ip, port = ip_port
+#             if re.match(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", ip):
+#                 result.append(f"{ip} {port}")
 
-    # 输出处理后的内容add到output file
-    with open(output_file, "a") as output_file:
-        for item in result:
-            output_file.write(item + "\n")
+#     # 输出处理后的内容add到output file
+#     with open(output_file, "a") as output_file:
+#         for item in result:
+#             output_file.write(item + "\n")
 
-    print("处理完成，fofa结果已保存到" + output_file.name)
+#     print("处理完成，fofa结果已保存到" + output_file.name)
 
 
-def getdb_append_to_file(program_path, data_file, merge_file):
-    # 添加可执行权限
-    try:
-        subprocess.run(["chmod", "+x", program_path], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error changing permissions for {program_path}: {e}")
-        return
-    try:
-        subprocess.run([program_path], check=True)
-        with open(data_file, "r") as data_file_obj:
-            data = data_file_obj.read()
-        with open(merge_file, "a") as merge_file_obj:
-            merge_file_obj.write(data)
-        print(f"DB Data from {data_file} has been appended to {merge_file}")
-        # 移除data_file文件
-        os.remove(data_file)
-        # print(f"{data_file} has been removed.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error running {program_path}: {e}")
+# def getdb_append_to_file(program_path, data_file, merge_file):
+#     # 添加可执行权限
+#     try:
+#         subprocess.run(["chmod", "+x", program_path], check=True)
+#     except subprocess.CalledProcessError as e:
+#         print(f"Error changing permissions for {program_path}: {e}")
+#         return
+#     try:
+#         subprocess.run([program_path], check=True)
+#         with open(data_file, "r") as data_file_obj:
+#             data = data_file_obj.read()
+#         with open(merge_file, "a") as merge_file_obj:
+#             merge_file_obj.write(data)
+#         print(f"DB Data from {data_file} has been appended to {merge_file}")
+#         # 移除data_file文件
+#         os.remove(data_file)
+#         # print(f"{data_file} has been removed.")
+#     except subprocess.CalledProcessError as e:
+#         print(f"Error running {program_path}: {e}")
 
 
 def remove_duplicate_lines(file_path):
-    with open(file_path, "r") as file:
+    """
+    Remove duplicate lines from a file.
+
+    Parameters:
+    - file_path (str): The path to the file.
+
+    Returns:
+    - None
+    """
+    with open(file_path, "r", encoding="utf-8") as file:
         lines = file.readlines()
+    # Rest of your code...
 
     # 使用集合(set)来去除重复行
     unique_lines = set(lines)
 
-    with open(file_path, "w") as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         file.writelines(unique_lines)
 
 
 def extract_ip_port_from_rss(url, output_file):
+    """
+    Extracts IP and port information from an RSS feed.
+
+    Args:
+        url (str): The URL of the RSS feed.
+        output_file (str): The file to which the extracted information will be written.
+
+    Returns:
+        None
+    """
     try:
         # 解析RSS订阅
         feed = feedparser.parse(url)
 
         # 打开文件以写入IP和Port信息
-        with open(output_file, "a") as f:
+        with open(output_file, "a", encoding="utf-8") as f:
             # 遍历每个RSS项
             for entry in feed.entries:
                 # 提取标题中的IP和Port信息
@@ -124,6 +144,16 @@ def extract_ip_port_from_rss(url, output_file):
 
 
 def download_and_convert(url, output_file):
+    """
+    Downloads content from the given URL and saves it to the specified output file.
+
+    Parameters:
+    - url (str): The URL from which to download the content.
+    - output_file (str): The file path where the downloaded content will be saved.
+
+    Returns:
+    None
+    """
     try:
         # 下载文本内容
         response = requests.get(url)
@@ -151,6 +181,15 @@ def download_and_convert(url, output_file):
 
 
 def move_files_to_current_directory(source_directory):
+    """
+    Move files from the specified source directory to the current working directory.
+
+    Parameters:
+    - source_directory (str): The path to the source directory containing the files to be moved.
+
+    Returns:
+    None
+    """
     try:
         # 获取当前目录
         target_directory = "./"
@@ -194,18 +233,29 @@ def unzip_file(zip_path, extract_folder):
 
 
 def merge_ip_files():
+    """
+    Merge multiple IP files.
+
+    This function reads a list of IP files in the current directory and merges them.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     ip_files = []
     for file_name in os.listdir():
         if file_name.endswith(".txt") and len(file_name.split("-")) == 3:
             ip_files.append(file_name)
 
     unique_ips = set()
-    with open("merge-ip.txt", "w") as ip_output:
+    with open("merge-ip.txt", "w", encoding="utf-8") as ip_output:
         for ip_file in ip_files:
             ip_parts = ip_file.split("-")
             ip = ip_parts[0]
             port = ip_parts[2].split(".")[0]  # Remove .txt extension
-            with open(ip_file, "r") as ip_input:
+            with open(ip_file, "r", encoding="utf-8") as ip_input:
                 for line in ip_input:
                     line = line.strip()
                     if line:  # Skip empty lines
@@ -213,7 +263,7 @@ def merge_ip_files():
                         if ip_port not in unique_ips:
                             unique_ips.add(ip_port)
                             ip_output.write(f"{ip_port}\n")
-            # os.remove(ip_file)  # Delete the original txt file
+            os.remove(ip_file)  # Delete the original txt file
 
 
 # def merge_ip_files2():
@@ -300,9 +350,9 @@ if __name__ == "__main__":
         # merge_ip_files2()
         merge_ip_files()
         # 使用示例：下载并转换数据
-        url = "https://sub.cfno1.eu.org/pure"
+        cfno1_url = "https://sub.cfno1.eu.org/pure"
         output_file = "merge-ip.txt"
-        download_and_convert(url, output_file)
+        download_and_convert(cfno1_url, output_file)
         # 指定RSS订阅的URL和输出文件名
         rss_url = os.environ.get("RSS_URL")
         # output_file = "ip_port.txt"
@@ -310,17 +360,17 @@ if __name__ == "__main__":
         extract_ip_port_from_rss(rss_url, output_file)
         # 使用示例，相同文件名作为输入和输出
         # getting cf ips from db sec
-        # 示例用法
-        program_path = "./cfip_db_linux_x64"
-        data_file = "db-data.txt"
-        merge_file = "merge-ip.txt"
-        getdb_append_to_file(program_path, data_file, merge_file)
+        # # 示例用法
+        # program_path = "./cfip_db_linux_x64"
+        # data_file = "db-data.txt"
+        # merge_file = "merge-ip.txt"
+        # getdb_append_to_file(program_path, data_file, merge_file)
         # # 从环境变量中获取fofa api URL
-        fofa_url = os.environ.get("FOFA_URL")
+        # fofa_url = os.environ.get("FOFA_URL")
 
-        if fofa_url:
-            process_fofa_file(fofa_url)
-        else:
-            print("未找到FOFA_URL环境变量")
+        # if fofa_url:
+        #     process_fofa_file(fofa_url)
+        # else:
+        #     print("未找到FOFA_URL环境变量")
         # file_path = 'merge-ip.txt'
         remove_duplicate_lines(output_file)
